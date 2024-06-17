30df8bb3ca9aa9bcc28b2b6ed993d064bfdcc66a
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2024-06-17 20:38:48 +0200

    Added razor pages

--- StoreASP/Program.cs
+++ StoreASP/Program.cs
@@ -8,7 +8,7 @@ namespace StoreASP
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
+            builder.Services.AddRazorPages();
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
@@ -21,6 +21,8 @@ namespace StoreASP
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.MapRazorPages();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
